7ffb5b52b810cde06648cb1a3a5c92af20683ce0
Felix Krause <contact@flyx.org>
2022-03-11 21:50:05 +0000

    vastly simpler latex behavior
     * use displayline utility to launch Skim
     * removed issue reporting (provided by langserver in the future)
     * added settings
     * simplified latex tasks
     * always provide an autogenerated task.
       if latexmkrc has default_files, that will be used as task.
       otherwise, current file will be rendered.

--- Scripts/main.js
+++ Scripts/main.js
@@ -23,31 +23,142 @@ nova.commands.register("org.flyx.tex.skim-setup", (workspace) => tex_suite.displ
 nova.commands.register("org.flyx.tex.emph", wrapWith("\\emph{", "{\\em "));
 nova.commands.register("org.flyx.tex.bold", wrapWith("\\textbf{", "{\\bf "));
 
-nova.assistants.registerIssueAssistant(["tex", "latex", "context"], {
-	provideIssues: function(editor) {
-		const skim_preview = tex_suite.getSkimPreview();
-		if (skim_preview == null) return [];
-		skim_preview.regenerate();
-    return [];
+class LatexTaskProvider {
+	static identifier = "org.flyx.tex.latex.tasks";
+	
+	static reload() {
+		nova.workspace.reloadTasks(this.identifier);
 	}
-}, {event: "onSave"});
-
-function findRcFile() {
-	let rc_path = nova.path.join(nova.workspace.path, "latexmkrc");
-	let stat = nova.fs.stat(rc_path);
-	if (stat == null) {
-		rc_path = nova.path.join(nova.workspace.path, ".latexmkrc");
-		stat = nova.fs.stat(rc_path);
+	
+	constructor() {
+		nova.config.onDidChange("org.flyx.tex.paths.skim", LatexTaskProvider.reload, this);
+		nova.config.onDidChange("org.flyx.tex.paths.latex", LatexTaskProvider.reload, this);
+		this.reload();
 	}
-	if (stat != null) {
-		if (!stat.isFile()) {
-			console.error("[.]latexmkrc in workspace is not a file!");
-			return null;
+	
+	reload() {
+		const dlpath = nova.path.join(nova.config.get("org.flyx.tex.paths.skim"), "/Contents/SharedSupport/displayline");
+		this.displayline = nova.fs.stat(dlpath) ? dlpath : null;
+		tex_suite.findTool("latexmk", "org.flyx.tex.paths.latex", false).then((path) => {
+			console.log("setting latexmk to " + path);
+			this.latexmk = path;
+			nova.workspace.reloadTasks(LatexTaskProvider.identifier);
+		});
+	}
+	
+	static latexmkOpts(...additional) {
+		return [
+			"-interaction=nonstopmode",
+			"-synctex=1",
+			"-cd",
+			...additional
+		];
+	}
+	
+	genericLatexmkTask() {
+		const task = new Task("Current LaTeX File");
+		task.setAction(Task.Build, new TaskProcessAction(this.latexmk, {
+			args: LatexTaskProvider.latexmkOpts("$(Config:org.flyx.tex.latex.engine)", "$File"),
+		}));
+		task.setAction(Task.Clean, new TaskProcessAction(this.latexmk, {
+			args: LatexTaskProvider.latexmkOpts("$(Config:org.flyx.tex.latex.engine)", "-c", "$File"),
+		}));
+		if (this.displayline) {
+			task.setAction(Task.Run, new TaskProcessAction(this.displayline, {
+				args: [
+					"$LineNumber",
+					"$FileDirname/${Command:org.flyx.tex.getFilenameWithoutExt}.pdf",
+					"$File"
+				],
+			}));
 		}
-		return rc_path;
+		return task;
 	}
-  return null;
+	
+	static findRcFile() {
+		let rc_path = nova.path.join(nova.workspace.path, "latexmkrc");
+		let stat = nova.fs.stat(rc_path);
+		if (stat == null) {
+			rc_path = nova.path.join(nova.workspace.path, ".latexmkrc");
+			stat = nova.fs.stat(rc_path);
+		}
+		if (stat != null) {
+			if (!stat.isFile()) {
+				console.error("[.]latexmkrc in workspace is not a file!");
+				return null;
+			}
+			return rc_path;
+		}
+	  return null;
+	}
+	
+	provideTasks() {
+		if (!this.latexmk) return null;
+		const rc_file = LatexTaskProvider.findRcFile();
+		let task = null;
+		if (rc_file != null) {
+			const content = nova.fs.open(rc_file);
+			let line;
+			let defines_files = false;
+			while (line = content.readline()) {
+				if (line.includes("@default_files")) {
+					defines_files = true;
+					break;
+				}
+			}
+			content.close();
+			if (defines_files) {
+				return new Promise((resolve, reject) => {
+					const proc = new Process(latexmk, {
+						args: [
+							"-cd",
+							"-r",
+							rc_file,
+							"-lualatex",
+							"-lualatex='echo org.flyx.tex.latexmk: %A %D'"
+						]
+					});
+					let defined_files = {};
+					proc.onStdout((line) => {
+						const data = /^org.flyx.tex.latexmk: "((?:\\"|[^"])*)" "((?:\\"|[^"])*)$"/.match(line);
+						if (data != null) {
+							defined_files[data[1] + ".tex"] = data[2];
+						}
+					});
+					proc.onExit((status) => {
+						let tasks = [];
+						for (const [key, value] of defined_files.entries()) {
+							let task = new Task(value);
+							task.setAction(Task.Build, new TaskProcessAction(this.latexmk, {
+								args: LatexTaskProvider.latexmkOpts("-r", rc_file, key)
+							}));
+							task.setAction(Task.Clean, new TaskProcessAction(this.latexmk, {
+								args: LatexTaskProvider.latexmkOpts("-c", "-r", rc_file, key)
+							}));
+							if (this.displayline) {
+								task.setAction(Task.Run, new TaskProcessAction(this.displayLine, {
+									args: [
+										"$LineNumber",
+										value,
+										key
+									]
+								}));
+							}
+						}
+						resolve(tasks.length > 0 ? tasks : [this.genericLatexmkTask()]);
+					});
+				});
+			}
+		}
+		return [this.genericLatexmkTask()];
+	}
 }
+
+nova.assistants.registerTaskAssistant(new LatexTaskProvider(), {
+	identifier: LatexTaskProvider.identifier
+});
+
+nova.commands.register('org.flyx.tex.getFilenameWithoutExt', (workspace) => nova.path.splitext(workspace.activeTextEditor.document.path)[0]);
 
 function handleError(msg) {
 	if (msg == null) return null; // should never happen
@@ -59,41 +170,6 @@ function handleError(msg) {
 		args: args
 	});
 }
-
-nova.assistants.registerTaskAssistant({
-	provideTasks: function() {
-		const rc_file = findRcFile();
-		if (rc_file != null) {
-			let task = new Task("latexmkrc");
-			task.setAction(Task.Build, new TaskResolvableAction({data: "latexmkrc"}));
-			task.setAction(Task.Run, new TaskResolvableAction({data: "latexmkrc"}));
-			task.setAction(Task.Clean, Latexmk.cleanProcess());
-			return [task];
-		}
-		return null;
-	},
-	resolveTaskAction: function(context) {
-		const processor = context.config ? (context.config.get("tex.latex.processor") || "") : "";
-		const mainfile = context.config ? (context.config.get("tex.latex.mainfile") || "") : "";
-		console.log("processor=" + processor + ", mainfile=" + mainfile + ", context=");
-		console.log(context.config);
-		if (context.action == Task.Build) {
-			let latexmk = new Latexmk(nova.workspace.path, processor, mainfile);
-			return latexmk.run("workspace").then(() => {
-				return new TaskProcessAction("/usr/bin/true", {args: []});
-			}).catch((msg) => handleError(msg));
-		} else if (context.action == Task.Run) {
-			const my_workspace = nova.workspace;
-			return skim.setupTmpDir().then((tmp_dir) => {
-				const skim_preview = new skim.Preview(tmp_dir, new Latexmk(tmp_dir, processor, mainfile), my_workspace);
-				skim_preview.regenerate();
-				return skim_preview.action();
-			});
-		} else if (context.action == Task.Clean) {
-			return Latexmk.cleanProcess();
-		}
-	}
-}, {identifier: "org.flyx.tex.tasks.latexmk"});
 
 nova.assistants.registerTaskAssistant({
 	provideTasks: function() { return null; },
--- Scripts/tex_suite.js
+++ Scripts/tex_suite.js
@@ -1,96 +1,52 @@
 // this module is the data singleton of the TeX Suite extension.
 
-const state = {
-	skim_preview: null,
-	
-	dispose: function() {
-		if (this.skim_preview != null) {
-			this.skim_preview.navigate.terminate();
-		}
-	}
-};
-
-function findTool(name, config_name, get_dir) {
-	let msg = "searching path for tool " + name + "… ";
-	return new Promise((resolve, reject) => {
-		let path = nova.workspace.config.get(config_name);
-		if (path) {
-			msg += "found at workspace config: ";
-		} else {
-			path = nova.config.get(config_name);
-			if (path) {
-				msg += "found at global config: ";
-			} else {
-				let p = new Process("/bin/bash", {
-					args: [nova.path.join(nova.extension.path, "Scripts", "find_tool.sh"), name]
-				});
-				p.start();
-				p.onStdout((line) => {
-					path = get_dir ? nova.path.dirname(line.trim()) : line.trim();
-				});
-				p.onDidExit((status) => {
-					if (status == 0) {
-						console.log(msg + "found by probing shell: " + path);
-						resolve(path);
-					}
-					else reject("could not find tool `" + name + "` in your PATH! configure its location in your workspace or global preferences.");
-				});
-				return;
-			}
-		}
-		console.log(msg + path);
-		resolve(path);
-	});
-}
-
 module.exports = {
-	displaySkimSetup: function() {
-		nova.workspace.showInformativeMessage(`
-This extension allows you to use the Skim PDF viewer to continuously preview \
-your PDF. If you want to use backwards navigation (from PDF to source), you \
-need to tell Skim how to communicate with Nova. To do this, setup Skim's Sync \
-settings as follows:
-
-Preset: Custom
-Command: /bin/sh
-Arguments: "${nova.path.join(nova.extension.path, "Scripts", "navigate_source.sh")}" "%file" %line
-
-Heads up: Nova currently lacks an API for setting the active editor. \
-Therefore, navigation uses AppleScript to do so. The first time you're using \
-this feature, macOS will prompt you to enable Nova.app to use Accessibility \
-features in the Security & Privacy System Preferences. If you don't do this, \
-navigation cannot activate the editor containing the source file to which you \
-want to navigate.
-
-This message is displayed once when installing TeX Suite \
-or updating to >= v0.3.0, and can later be accessed via the Extensions menu.`);
-	},
-	
 	activate: function() {
 		console.log("Activating TeX Suite");
-		nova.subscriptions.add(state);
-		const storage = nova.extension.globalStoragePath;
-		if (nova.fs.stat(storage) == null) {
-			nova.fs.mkdir(storage);
-		}
-		const indicator = nova.path.join(storage, "seen_skim_note.ind");
-		if (nova.fs.stat(indicator) == null) {
-			const file = nova.fs.open(indicator, "w");
-			file.close();
-			this.displaySkimSetup();
-		}
+		nova.config.onDidChange('org.flyx.tex.paths.context', this.reload, this);
+		this.reload();
 	},
 	deactivate: function() {
 		console.log("Unloading TeX Suite");
-		nova.subscriptions.remove(state);
 	},
 	
-	latexPath: function() {
-		return findTool("latexmk", "org.flyx.tex.paths.latex", true);
+	findTool: function(name, config_name, get_dir) {
+		let msg = "searching path for tool " + name + "… ";
+		return new Promise((resolve, reject) => {
+			let path = nova.workspace.config.get(config_name);
+			if (path) {
+				msg += "found at workspace config: ";
+			} else {
+				path = nova.config.get(config_name);
+				if (path) {
+					msg += "found at global config: ";
+				} else {
+					let p = new Process("/bin/bash", {
+						args: [nova.path.join(nova.extension.path, "Scripts", "find_tool.sh"), name]
+					});
+					p.start();
+					p.onStdout((line) => {
+						path = get_dir ? nova.path.dirname(line.trim()) : line.trim();
+					});
+					p.onDidExit((status) => {
+						if (status == 0) {
+							console.log(msg + "found by probing shell: " + path);
+							resolve(path);
+						}
+						else reject("could not find tool `" + name + "` in your PATH! configure its location in your workspace or global preferences.");
+					});
+					return;
+				}
+			}
+			console.log(msg + path);
+			resolve(path);
+		});
 	},
 	
-	contextPath: function() {
-		return findTool("context", "org.flyx.tex.paths.context", false);
+	reload: function() {
+		this.findTool("context", "org.flyx.tex.paths.context", true).then((path) => {
+			this.context = path;
+		});
 	},
 	
 	previewNotify: function(title, msg) {
@@ -104,14 +60,4 @@ or updating to >= v0.3.0, and can later be accessed via the Extensions menu.`);
 	previewNotifyDismiss: function() {
 		nova.notifications.cancel("org.flyx.tex.preview");
 	},
-	
-	setSkimPreview: function(value) {
-		state.skim_preview = value;
-	},
-	
-	getSkimPreview: function() {
-		return state.skim_preview;
-	},
-	
-	issues: new IssueCollection()
 }
