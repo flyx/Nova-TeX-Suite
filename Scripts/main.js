064384ea323f4f47e4a80c8f3a0d6393ca563dd1
Felix Krause <contact@flyx.org>
2022-03-14 21:03:25 +0100

    integrated texlab language server

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,15 @@
+let langserver = null;
+
 exports.activate = () => {
 	console.log("activiting TeX Suite");
+	langserver = new LatexLanguageServer();
 }
 exports.deactivate = () => {
 	console.log("deactiviting TeX Suite");
+	if (langserver) {
+			langserver.deactivate();
+			langserver = null;
+	}
 }
 
 function wrapWith(latex, context) {
@@ -206,6 +213,64 @@ class LatexTaskProvider {
 nova.assistants.registerTaskAssistant(new LatexTaskProvider(), {
 	identifier: LatexTaskProvider.identifier
 });
+
+class LatexLanguageServer {
+	static identifier = "org.flyx.tex.latex.server";
+	
+	constructor() {
+		// Observe the configuration setting for the server's location, and restart the server on change
+		nova.config.observe("org.flyx.tex.paths.latex.server", function(path) {
+			this.start(path);
+		}, this);
+	}
+		
+	deactivate() {
+		this.stop();
+	}
+		
+	start() {
+		if (this.languageClient) {
+				this.languageClient.stop();
+				nova.subscriptions.remove(this.languageClient);
+		}
+		
+		findTool("texlab", "org.flyx.tex.paths.latex.server", false).then((path) => {
+			// Create the client
+			var serverOptions = {
+				path: path,
+				args: ['-v']
+			};
+			var clientOptions = {
+				// The set of document syntaxes for which the server is valid
+				syntaxes: ['latex']
+			};
+			var client = new LanguageClient("org.flyx.tex.latex", "LaTeX Language Server", serverOptions, clientOptions);
+			
+			try {
+				console.log("starting LaTeX language server");
+				// Start the client
+				client.start();
+				
+				// Add the client to the subscriptions to be cleaned up
+				nova.subscriptions.add(client);
+				this.languageClient = client;
+			} catch (err) {
+				// If the .start() method throws, it's likely because the path to the language server is invalid
+				if (nova.inDevMode()) {
+						console.error(err);
+				}
+			}
+		});
+	}
+		
+	stop() {
+		if (this.languageClient) {
+			this.languageClient.stop();
+			nova.subscriptions.remove(this.languageClient);
+			this.languageClient = null;
+		}
+	}
+}
 
 class ContextTaskProvider {
 	static identifier = "org.flyx.tex.context.tasks";
